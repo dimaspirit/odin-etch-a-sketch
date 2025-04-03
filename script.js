34c4d690a02cd157505014d7b664ac2d2a7d06b3
Dmytro Lobanov <dspwww@gmail.com>
2025-04-03 17:50:49 +0100

    feat: Add reset and fill squires in grid

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 console.log('Hello, World!');
 
 const gridContainerNode = document.querySelector('.grid-container');
-const rows = 4;
-const columns = 4;
+const resizeBtn = document.getElementById('resize');
+const resetBtn = document.getElementById('reset');
 
-function createGrid() {
-  const gridElementsCount = 16*16;
+const squareSize = 16;
+const squareNumberDefault = 40;
 
-  for (let index = 0; index < gridElementsCount; index++) {
+resizeBtn.addEventListener('click', (event) => {
+  const userInput = prompt('Enter the number of squares per side, max is 100');
+
+  if(userInput === null) {
+    return;
+  }
+
+  if(isNaN(+userInput)) {
+    alert('It should be a number');
+    return;
+  }
+
+  if(+userInput > 100) {
+    alert('A max of a number is 100');
+    return;
+  }
+
+  while (gridContainerNode.firstChild) {
+    gridContainerNode.lastChild.removeEventListener('mouseenter', makeNodeActive);
+    gridContainerNode.removeChild(gridContainerNode.lastChild);
+  }
+
+  createGrid(+userInput);
+});
+
+resetBtn.addEventListener('click', (event) => {
+  console.log('Reset');
+  const squires = document.querySelectorAll('.node');
+
+  squires.forEach(squire => squire.classList.remove('active'));
+});
+
+function makeNodeActive(event) {
+  event.target.classList.add('active');
+}
+
+function createGrid(squaresNumber) {
+  gridContainerNode.style.width = `${squaresNumber*squareSize}px`;
+
+  for (let index = 0; index < squaresNumber*squaresNumber; index++) {
     const node = document.createElement('div');
+
     node.classList.add('node');
+    node.addEventListener('mouseenter', makeNodeActive);
+
     gridContainerNode.appendChild(node);
   }
 }
 
-createGrid();
+createGrid(squareNumberDefault);
